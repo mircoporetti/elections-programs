9daa2580672c3ebb9f29547579e4c9e53db8c2fd
Mirco Poretti <mircoporetti@gmail.com>
2025-02-03 19:11:00 +0100

    Test and refactor history recognition

--- src/chat/ai_assistant.py
+++ src/chat/ai_assistant.py
@@ -2,66 +2,60 @@ import os
 import re
 from typing import List, Dict
 
-from chat.party import Party
-from store.vector_store import similarity_search_for
-from langchain_huggingface import HuggingFaceEndpoint
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.messages import AIMessage, HumanMessage
+from langchain_huggingface import HuggingFaceEndpoint
 
+from chat.party import Party
 from store import vector_store
+from store.vector_store import similarity_search_for
+
 
 huggingface_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 if not huggingface_token:
     raise ValueError("HUGGINGFACEHUB_API_TOKEN environment variable is not set.")
 
 system_prompt = (
-    "You are a concise AI assistant, expert in politics."
-    "Respond in plain text without repeating the same sentence."
-    "Use the given context to answer the question."
-    "If you are unsure about the answer, say you don't know."
-    "Limit your response to max three concise sentences."
-    "The party object of the question is: {party}."
+    "You are a concise AI assistant, expert in politics. "
+    "Respond in plain text without repeating the same sentence or adding labels like 'AI:', 'Assistant:', or 'Human: "
+    "Use the given context to answer the question. "
+    "If you are unsure about the answer, say you don't know. "
+    "Limit your response to max three concise sentences. "
+    "The party object of the question is: {party}. "
     "Context: {context}"
 )
 
-prompt_template = ChatPromptTemplate.from_messages([
-    ("system", system_prompt),
-    MessagesPlaceholder("chat_history"),
-    ("human", "{input}"),
-])
 
 generative_model = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.3", temperature=0.7)
 
 
 def answer(question: str, history: List[Dict[str, str]]):
-    converted_history = []
-    for msg in history:
-        if msg["role"].lower() == "ai":
-            converted_history.append(AIMessage(content=msg["content"]))
-        else:
-            converted_history.append(HumanMessage(content=msg["content"]))
-
-    recent_history = converted_history[-2:]
+    recent_history = [
+        AIMessage(content=msg["content"]) if msg["role"].lower() == "ai" else HumanMessage(content=msg["content"])
+        for msg in history[-4:]
+    ]
 
     prompt = ChatPromptTemplate.from_messages([
         ("system", system_prompt),
-        *recent_history,
-        ("human", question),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("human", "{input}"),
     ])
     party = Party.get_from_history(history)
+
     question_answer_chain = create_stuff_documents_chain(generative_model, prompt)
     retriever = vector_store.get_store_as_retriever_for(party)
     chain = create_retrieval_chain(retriever, question_answer_chain)
 
-    response = chain.invoke({"input": question, "party": party.name})
+    response = chain.invoke({"input": question, "party": party.name, "chat_history": recent_history})
     llm_answer = response["answer"].strip()
-    return re.sub(r'^(AI|Assistant|Bot|System):\s*', '', llm_answer, flags=re.IGNORECASE)
+    return re.sub(r'(AI|Assistant|Bot|System|Human|):\s*', '', llm_answer, flags=re.IGNORECASE)
 
 
 def answer_with_most_pertinent_chunks(question: str):
     best_chunks = similarity_search_for(Party.get_from_message(question), question)
 
     return best_chunks
+
 
